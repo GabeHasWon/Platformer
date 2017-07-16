588144e677f92f2cefe3804d280d1f2bd3ccc925
GabeHasWon <gabriel@guevarameyer.com>
2017-07-16 15:36:03 -0500

    Uploaded main code

---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    public static class Extensions
+    {
+        public static float NextFloat(this Random rand)
+        {
+            return (float)(rand.NextDouble());
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Platformer.Drawing;
+using Platformer.Objects;
+using Platformer.WorldGen;
+using System;
+
+namespace Platformer
+{
+    public class Main : Game
+    {
+        GraphicsDeviceManager graphics;
+        public SpriteBatch batch;
+        public static Player player;
+        public static Random rand = new Random();
+        public static Vector2 worldPosition = new Vector2(0);
+        public static int Right = 0;
+
+        public Main()
+        {
+            Right = Window.ClientBounds.Right;
+            player = new Player();
+            Obstacle.SetObstacles();
+            Dust.SetDusts();
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Generator.MakeWorld();
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += new EventHandler<EventArgs>(Window_ClientSizeChanged);
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Textures.Load(Content);
+            batch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Right = Window.ClientBounds.Right;
+            graphics.ApplyChanges();
+            player.Update();
+            Obstacle.UpdateAll();
+            Dust.UpdateAll();
+            if (graphics.PreferredBackBufferWidth != Window.ClientBounds.Width)
+            {
+                graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
+                graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
+                graphics.ApplyChanges();
+            }
+            if (Player.KeyDown(Keys.N))
+                Dust.AddDust(Mouse.GetState().Position.ToVector2(), 400, new Vector2(0, 2 + rand.NextFloat()));
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            batch.Begin(SpriteSortMode.BackToFront, BlendState.NonPremultiplied, SamplerState.PointClamp);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            player.Draw(batch);
+            Obstacle.DrawAll(batch);
+            Dust.DrawAll(batch);
+            base.Draw(gameTime);
+            batch.End();
+        }
+
+        private void Window_ClientSizeChanged(object sender, EventArgs e)
+        {
+            if (graphics.PreferredBackBufferWidth != Window.ClientBounds.Width)
+            {
+                graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
+                graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
+            }
+        }
+    }
+}
