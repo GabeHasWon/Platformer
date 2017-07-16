de6def21c39345cf88db9b759dae448f1b3522d0
GabeHasWon <gabriel@guevarameyer.com>
2017-07-16 16:56:06 -0500

    added benchmark

--- WorldGen/Generator.cs
+++ WorldGen/Generator.cs
@@ -1,34 +1,19 @@
+using BenchmarkDotNet.Attributes;
 using Microsoft.Xna.Framework;
 using Platformer.Objects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Platformer.WorldGen
 {
     class Generator
     {
-        public static void MakeWorld()
+        [Benchmark]
+        static void MakeWorld()
         {
             int startY = 0;
-            for (int i = 0; i < 300; ++i)
+            for (int i = 0; i < 800; ++i)
             {
                 Obstacle.AddObstacle(new Vector2(i * 20, 400 - startY), 0);
-                for (int k = 0; k < 30; ++k)
-                {
-                    int randStone = Main.rand.Next(20);
-                    if (k > 5 && k <= 10)
-                        randStone = Main.rand.Next(16);
-                    if (k > 10 && k <= 21)
-                        randStone = Main.rand.Next(6);
-                    if (k > 21 && k <= 27)
-                        randStone = Main.rand.Next(3);
-                    if (k > 27)
-                        randStone = Main.rand.Next(1);
-                    Obstacle.AddObstacle(new Vector2(i * 20, 420 - startY + (k * 20)), randStone == 0 ? 2 : 1);
-                }
+                BuildUnderground(i, startY);
                 if (Main.rand.Next(20) == 0)
                 {
                     int size = Main.rand.Next(5, 9);
@@ -44,6 +29,23 @@ namespace Platformer.WorldGen
                     startY--;
                 if (rand == 1)
                     startY++;
+            }
+        }
+
+        static void BuildUnderground(int i, int startY)
+        {
+            for (int k = 0; k < 100; ++k)
+            {
+                int randStone = Main.rand.Next(20);
+                if (k > 5 && k <= 10)
+                    randStone = Main.rand.Next(16);
+                if (k > 10 && k <= 21)
+                    randStone = Main.rand.Next(6);
+                if (k > 21 && k <= 27)
+                    randStone = Main.rand.Next(3);
+                if (k > 27)
+                    randStone = Main.rand.Next(1);
+                Obstacle.AddObstacle(new Vector2(i * 20, 420 - startY + (k * 20)), randStone == 0 ? 2 : 1);
             }
         }
     }
